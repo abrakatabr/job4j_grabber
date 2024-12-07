17f10927eedbfad0b150e08e164394b92427df91
Pozhar <abrakatabr@mail.ru>
2024-12-07 19:52:07 +0300

    Web [#289478]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,15 +1,12 @@
 package ru.job4j;
 
 import org.apache.log4j.Logger;
-import ru.job4j.grabber.model.Post;
 import ru.job4j.grabber.service.Config;
-import ru.job4j.grabber.service.HabrCareerParse;
 import ru.job4j.grabber.service.SchedulerManager;
+import ru.job4j.grabber.service.Web;
 import ru.job4j.grabber.service.SuperJobGrab;
 import ru.job4j.grabber.stores.JdbcStore;
-import ru.job4j.grabber.stores.MemStore;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -30,6 +27,7 @@ public class Main {
                     Integer.parseInt(config.get("rabbit.interval")),
                     SuperJobGrab.class,
                     store);
+            new Web(store).start(Integer.parseInt(config.get("server.port")));
         } catch (SQLException e) {
             LOG.error("When create a connection", e);
             }
